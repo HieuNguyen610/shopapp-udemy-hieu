76ce910f5afdb78eafd7a1713da7e09ebcc7036d
hieunm2 <hieunm2@vnext.vn>
2025-03-11 10:30:58 +0700

    init commit

---
+++ src/main/java/hieu/shopappudemyhoang/ShopappUdemyHoangApplication.java
@@ -0,0 +1,13 @@
+package hieu.shopappudemyhoang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopappUdemyHoangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopappUdemyHoangApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hieu/shopappudemyhoang/ShopappUdemyHoangApplicationTests.java
@@ -0,0 +1,13 @@
+package hieu.shopappudemyhoang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopappUdemyHoangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
